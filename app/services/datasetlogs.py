f243a3ced868ad1690f3f53b567cd59d3fcc7079
coderAung <yewontaung798@gmail.com>
2026-07-23 22:07:58 +0630

    feat: Update dataset log to response in pagination result

--- app/services/datasetlogs.py
+++ app/services/datasetlogs.py
@@ -1,20 +1,24 @@
 from sqlalchemy.orm import selectinload
-from sqlmodel import Session, select
+from sqlmodel import Session, func, select
 
 from app.data.models import Account, DatasetModificationLog
 from app.dtos.outputs import DatasetModificationLogListItem
+from app.utils.paginations import PaginationResult
 
 
-def search(page:int, size:int, session:Session) -> list[DatasetModificationLogListItem]:
+def search(page:int, size:int, session:Session) -> PaginationResult[DatasetModificationLogListItem]:
     query = select(
         DatasetModificationLog,
         Account).select_from(DatasetModificationLog).join(
             Account, Account.account_id == DatasetModificationLog.account_id
         ).limit(size).offset((page - 1) * size)
 
+    count = select(func.count(DatasetModificationLog.log_id))
+
     result = session.exec(query).all()
+    total = session.exec(count).one_or_none()
     
-    return [DatasetModificationLogListItem(
+    items = [DatasetModificationLogListItem(
         log_id=log.log_id,
         dataset_id=log.dataset_id,
         account_id=log.account_id,
@@ -25,3 +29,10 @@ def search(page:int, size:int, session:Session) -> list[DatasetModificationLogLi
         modification_type=log.modification_type,
         modified_at=log.created_at,
     ) for log, account in result]
+
+    return PaginationResult(
+        items=items,
+        page=page,
+        size=size,
+        total=total or 0
+    )
